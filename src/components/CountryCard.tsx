88d026026416632624022029450a0f111f44401e
Zivanchy <zivanmadjarev@gmail.com>
2022-07-10 21:01:06 +0200

    added onclick event for country card that leads to country page

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-interface Country {
+import { Link } from 'react-router-dom';
+
+export interface Country {
   name: string;
   capital: string;
   population: number;
   region: string;
   img: string;
+  cca3: string;
 }
 
 const CountryCard: React.FC<Country> = ({
@@ -14,28 +17,31 @@ const CountryCard: React.FC<Country> = ({
   population,
   region,
   img,
+  cca3,
 }) => {
   return (
-    <div className="card card-normal card-bordered bg-slate-800 w-72 shadow-md ease-in-out duration-200 hover:scale-105 hover:cursor-pointer">
-      <figure className="w-72 h-40 max-h-full object-cover">
-        <img src={img} alt="Country flag" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title text-slate-50">{name}</h2>
-        <p>
-          <span className="font-bold text-slate-50 mr-2">Population:</span>
-          {population.toLocaleString('en-US')}
-        </p>
-        <p>
-          <span className="font-bold text-slate-50 mr-2">Region:</span>
-          {region}
-        </p>
-        <p>
-          <span className="font-bold text-slate-50 mr-2">Capital:</span>
-          {capital}
-        </p>
+    <Link to={`/country/${cca3}`}>
+      <div className="card card-normal card-bordered bg-slate-800 w-72 shadow-md ease-in-out duration-200 hover:scale-105 hover:cursor-pointer">
+        <figure className="w-72 h-40 max-h-full object-cover">
+          <img src={img} alt="Country flag" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-slate-50">{name}</h2>
+          <p>
+            <span className="font-bold text-slate-50 mr-2">Population:</span>
+            {population.toLocaleString('en-US')}
+          </p>
+          <p>
+            <span className="font-bold text-slate-50 mr-2">Region:</span>
+            {region}
+          </p>
+          <p>
+            <span className="font-bold text-slate-50 mr-2">Capital:</span>
+            {capital}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
